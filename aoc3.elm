27580229dfd86535b5f22684f1c5df28fd65684f
Tomas Lieberkind <t.lieberkind@gmail.com>
2016-12-13 03:37:22 +0100

    Made aoc3 into a Html.program (to be able to load input dynamically)

--- aoc3.elm
+++ aoc3.elm
@@ -1,8 +1,26 @@
-module AOC3 exposing (solvePt1)
+import Http
+import Html exposing (..)
+import Json.Decode as Decode
+
+
+
+main =
+    Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+
+-- AOC3 SOLUTION
+
 
 type Candidate
     = Triple Int Int Int
     | InvalidCandidate
+
 
 toCandidate : String -> Candidate
 toCandidate str =
@@ -18,19 +36,88 @@ isPossibleTriangle : Int -> Int -> Int -> Bool
 isPossibleTriangle a b c =
     a + b > c && a + c > b && b + c > a
 
+
 isCandidateValid : Candidate -> Bool
 isCandidateValid cand =
     case cand of
         Triple x y z     -> isPossibleTriangle x y z
         InvalidCandidate -> False
 
-input : List String
-input =
-    ["  10  20  29"]
 
-solvePt1 : Int
-solvePt1 =
-    input
+solvePt1 : List String -> Int
+solvePt1 strs =
+    strs
     |> List.map toCandidate
     |> List.filter isCandidateValid
     |> List.length
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { result : Int
+    , loading : Bool
+    }
+
+
+init : (Model, Cmd Msg)
+init =
+    (Model 0 True, fetchInput)
+
+
+
+-- UPDATE
+type Msg
+    = FetchInput
+    | InputLoaded (Result Http.Error (List String))
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        FetchInput ->
+            ({ model | loading = True}, fetchInput)
+        
+        InputLoaded (Ok strs) ->
+            (Model (solvePt1 strs) False, Cmd.none)
+
+        InputLoaded (Err _) ->
+            (model, Cmd.none)
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  h2 [] [text (if model.loading then "Loading..." else (toString model.result))]
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- HTTP
+
+
+fetchInput : Cmd Msg
+fetchInput =
+    let 
+        url =
+            "https://raw.githubusercontent.com/lieberkind/adventofcode2016/master/aoc3.input.json"
+    in
+        Http.send InputLoaded (Http.get url decodeStringList)
+
+
+decodeStringList : Decode.Decoder (List String)
+decodeStringList =
+    (Decode.list Decode.string)
